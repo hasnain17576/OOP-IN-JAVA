43742b4b7b089af4645ba56c8db87e8e579ce664
Muhammad Hasnain <hasnainwasli17@gmail.com>
2025-09-25 22:15:44 +0500

    submit

---
+++ Students3/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Student s1=new Student();
+        s1.setName("hasnain");
+        s1.setAge(23);
+        System.out.println("Name"+s1.getName());
+        System.out.println("Age"+s1.getAge());
+
+    }
+}
